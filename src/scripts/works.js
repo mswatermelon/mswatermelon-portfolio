eba3e481c14f75b5b7993c16d4267bbfbdaf9d97
v.aristarkhova <Veronika.Aristarkhova@acronis.com>
2020-05-17 20:32:37 +0300

    Start implement works on vue

--- src/scripts/works.js
+++ src/scripts/works.js
@@ -1,6 +1,28 @@
 import Vue from "vue";
 
+const btns = {
+    template: "#works-btns"
+};
+const thumbs = {
+    template: "#works-thumbs"
+};
+
+const  visual = {
+    template: "#works-visual",
+    components: { thumbs, btns }
+};
+
+const tags = {
+    template: "#works-tags"
+};
+
+const info = {
+    template: "#works-info",
+    components: { tags }
+};
+
 new Vue({
-   el: "#works-component",
-    template: "#preview-container"
+    el: "#works-component",
+    template: "#preview-container",
+    components: { visual, info }
 });
